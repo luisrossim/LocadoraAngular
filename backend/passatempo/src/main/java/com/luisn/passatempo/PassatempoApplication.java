9d370bcdfd9e93f36d2cf44d4dc9473b7269ce98
luisrossim <luisrossim23@gmail.com>
2023-09-12 21:28:26 -0300

    init spring

---
+++ backend/passatempo/src/main/java/com/luisn/passatempo/PassatempoApplication.java
@@ -0,0 +1,13 @@
+package com.luisn.passatempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassatempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassatempoApplication.class, args);
+	}
+
+}
---
+++ backend/passatempo/src/test/java/com/luisn/passatempo/PassatempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisn.passatempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassatempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
